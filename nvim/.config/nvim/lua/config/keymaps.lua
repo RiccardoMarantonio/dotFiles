ca4bfb58082bb37dfe8fe76b7e33bf72502ea825
Marantz01 <riccardo.marantonio@gmail.com>
2025-05-10 18:57:03 +0200

    added snippets

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -17,3 +17,4 @@ end, { desc = "Fuzzy Change Directory" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
