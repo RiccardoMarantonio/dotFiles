728bb3459dcf6a3bde4548aaeffe2f2eef9ab1b6
Marantz01 <riccardo.marantonio@gmail.com>
2025-06-09 13:18:42 +0200

    commit

--- nvim/.config/nvim/lua/codeSeparator.lua
+++ nvim/.config/nvim/lua/codeSeparator.lua
@@ -15,6 +15,8 @@ local function repeat_char(char, count)
   return string.rep(char, count)
 end
 
+-- ########## hfdtfyuhlgkyjftd ##########
+
 -- Get comment prefix/suffix for current filetype
 local function get_comment_wrappers()
   local commentstring = vim.bo.commentstring -- e.g. "// %s", "# %s", "/* %s */"
@@ -58,7 +60,7 @@ function M.line_separator()
     end
 
     local text = " " .. input .. " "
-    local hashes = repeat_char(config.char, 5)
+    local hashes = repeat_char(config.char, 10)
     local line = hashes .. text .. hashes
 
     local lines = { line }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,1 +1,76 @@
-print("HElloooooooo")
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font_size = 15
+config.color_scheme = "Afterglow"
+-- config.color_scheme = "GitHub Dark"
+
+config.font = wezterm.font("JetBrains Mono")
+
+config.window_decorations = "NONE | RESIZE"
+
+config.enable_tab_bar = false
+config.tab_bar_at_bottom = true
+
+local act = wezterm.action
+
+-- config.window_background_opacity = 0.70
+-- config.macos_window_background_blur = 20
+
+config.keys = {
+	{
+		key = "|",
+		mods = "SUPER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = '"',
+		mods = "SUPER|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "w",
+		mods = "SUPER",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		key = "W",
+		mods = "SUPER|SHIFT",
+		action = wezterm.action.CloseCurrentTab({ confirm = false }),
+	},
+	{ key = "w", mods = "CTRL", action = act.PaneSelect },
+	-- Pane navigation (like `Ctrl-b h/j/k/l`)
+	{
+		key = "h",
+		mods = "CTRL",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "CTRL",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "CTRL",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "CTRL",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{ key = "RightArrow", mods = "ALT", action = wezterm.action.AdjustPaneSize({ "Right", 20 }) },
+	{ key = "LeftArrow", mods = "ALT", action = wezterm.action.AdjustPaneSize({ "Left", 20 }) },
+	{ key = "UpArrow", mods = "ALT", action = wezterm.action.AdjustPaneSize({ "Up", 10 }) },
+	{ key = "DownArrow", mods = "ALT", action = wezterm.action.AdjustPaneSize({ "Down", 10 }) },
+}
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 20,
+	bottom = 0,
+}
+
+return config
