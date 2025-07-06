ee9b6ca124ca5b863d8b493150f3f78454dce6f0
Marantz <riccardo.marantonio@gmail.com>
2025-07-06 02:42:10 +0200

    Better Copilot and new UI elements

--- nvim/.config/nvim/lua/plugins/AI.lua
+++ nvim/.config/nvim/lua/plugins/AI.lua
@@ -1,20 +1,34 @@
 return {
+  --   "zbirenbaum/copilot.lua",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup({
+  --       suggestion = {
+  --         enabled = true,
+  --         auto_trigger = true,
+  --         debounce = 75,
+  --         keymap = {
+  --           accept = "<Tab>", -- Tab accetta la suggestion
+  --           next = "<C-]>", -- suggerimento successivo
+  --           prev = "<C-[>", -- suggerimento precedente
+  --           dismiss = "<C-e>", -- chiude la suggestion
+  --         },
+  --       },
+  --       panel = { enabled = false },
+  --     })
+  --   end,
   "zbirenbaum/copilot.lua",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<Tab>", -- Tab accetta la suggestion
-          next = "<C-]>", -- suggerimento successivo
-          prev = "<C-[>", -- suggerimento precedente
-          dismiss = "<C-e>", -- chiude la suggestion
-        },
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 150,
+      keymap = {
+        accept = "<C-l>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
       },
-      panel = { enabled = false },
-    })
-  end,
+    },
+  },
 }
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,31 @@
 return {
-  "sainnhe/gruvbox-material",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Load the colorscheme
-    vim.g.gruvbox_material_enable_italic = true
-    vim.cmd.colorscheme("gruvbox-material")
-    -- Diagnostic text (virtual text shown at EOL)
-    vim.api.nvim_set_hl(
-      0,
-      "DiagnosticVirtualTextError",
-      { fg = "#ff5f5f", bg = "#2a0000", italic = true, undercurl = true }
-    )
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Load the colorscheme
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme("gruvbox-material")
+      -- Diagnostic text (virtual text shown at EOL)
+      vim.api.nvim_set_hl(
+        0,
+        "DiagnosticVirtualTextError",
+        { fg = "#ff5f5f", bg = "#2a0000", italic = true, undercurl = true }
+      )
 
-    vim.api.nvim_set_hl(0, "Pmenu", { bg = "#282828", fg = "#ebdbb2" })
-    vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#458588", fg = "#ffffff" })
-    vim.api.nvim_set_hl(0, "PmenuSbar", { bg = "#3c3836" })
-    vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#504945" })
-    vim.api.nvim_set_hl(0, "PmenuBorder", { fg = "#504945", bg = "#282828" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#282828" })
-    vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#504945", bg = "#282828" })
-    vim.api.nvim_set_hl(0, "CmpSelection", { bg = "#458588", fg = "#ffffff" })
-    -- vim.api.nvim_set_hl(0, "@type", { fg = "#d79921" })
-    -- vim.api.nvim_set_hl(0, "@type.definition", { fg = "#d79921" })
-    vim.api.nvim_set_hl(0, "@keyword", { fg = "#83a598", italic = true })
-    vim.cmd([[
+      vim.api.nvim_set_hl(0, "Pmenu", { bg = "#282828", fg = "#ebdbb2" })
+      vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#458588", fg = "#ffffff" })
+      vim.api.nvim_set_hl(0, "PmenuSbar", { bg = "#3c3836" })
+      vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#504945" })
+      vim.api.nvim_set_hl(0, "PmenuBorder", { fg = "#504945", bg = "#282828" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#282828" })
+      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#504945", bg = "#282828" })
+      vim.api.nvim_set_hl(0, "CmpSelection", { bg = "#458588", fg = "#ffffff" })
+      -- vim.api.nvim_set_hl(0, "@type", { fg = "#d79921" })
+      -- vim.api.nvim_set_hl(0, "@type.definition", { fg = "#d79921" })
+      vim.api.nvim_set_hl(0, "@keyword", { fg = "#83a598", italic = true })
+      vim.cmd([[
       hi Normal guibg=NONE ctermbg=NONE
       hi NormalNC guibg=NONE ctermbg=NONE
       hi NormalFloat guibg=NONE ctermbg=NONE
@@ -32,5 +33,26 @@ return {
       hi CursorLine guibg=NONE ctermbg=NONE
       hi EndOfBuffer guibg=NONE ctermbg=NONE
     ]])
-  end,
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true, -- Enable transparency
+        integrations = {},
+      })
+      -- vim.cmd.colorscheme("catppuccin-mocha") -- Or any other Catppuccin flavor
+      vim.cmd([[
+      hi Normal guibg=NONE ctermbg=NONE
+      hi NormalNC guibg=NONE ctermbg=NONE
+      hi NormalFloat guibg=NONE ctermbg=NONE
+      hi FloatBorder guibg=NONE ctermbg=NONE
+      hi CursorLine guibg=NONE ctermbg=NONE
+      hi EndOfBuffer guibg=NONE ctermbg=NONE
+    ]])
+    end,
+  },
 }
